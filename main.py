12647466b8057479d506600b7bff8607f753b5b3
mattiazza <mattiapiazza@gmail.com>
2025-10-17 12:13:22 +0200

    Fourth First Commit

--- main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("Hello from my internship!")
-    print("Excited to learn and contribute.")
-
-
-if __name__ == "__main__":
-    main()
---
+++ provami.py
@@ -0,0 +1,4 @@
+print("This is a test file.")
+for i in range(5):
+    print("This is iteration", i)
+print("End of the test file.")
